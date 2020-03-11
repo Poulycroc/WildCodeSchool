edb2eded697ca15c04032261b6bd33d6199b60b5
Boris Ahishakiye <borisahishakiye@Boriss-MBP.lan>
2020-03-11 21:34:47 +0100

    add a task in todo list

--- react/todoList/front/src/App.js
+++ react/todoList/front/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import TodosContainer from './components/TodosContainer'
+import TodosContainer from './components/TodosContainer';
+import TodoAdd from './components/TodoAdd'
 
 function App() {
+  const handleInputValue = (event) => {
+    console.log({handleInputValue: event})
+  }
+
   return (
     <div className="app-viewport">
       <div className="container">
+        <TodoAdd handleInputValue={handleInputValue}/>
         <TodosContainer />
       </div>
     </div>
