a9679a65fdc22a1ce6ad63f407333a526e958059
Maxime Bartier <maxime@wildcodeschool.com>
2020-03-17 01:30:08 +0100

    adding global axios

--- react/react-app-router/src/pages/Home.js
+++ react/react-app-router/src/pages/Home.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { Component } from "react";
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import axios from "axios";
 
-export default () => {
-  return (
-    <Container>
-      <Row>
-        <Col>Home page</Col>
-      </Row>
-    </Container>
-  );
-};
+class HomePage extends Component {
+  state = {
+    todos: [],
+    error: false
+  };
+
+  renderTodos() {
+    return this.state.todos.map((item, key) => (
+      <ListGroup.Item key={key}>{item.title}</ListGroup.Item>
+    ));
+  }
+
+  async componentDidMount() {
+    const { data } = await axios.get("todos");
+    this.setState({ todos: data });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>Home page</Col>
+        </Row>
+        <Row>
+          <Col>
+            <ListGroup>{this.renderTodos()}</ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default HomePage;
